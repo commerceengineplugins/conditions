50e77c6195c5d78c56634f6d8e3f36a8bbef61fd
Erwin Werkman <werkman.erwin@gmail.com>
2017-05-15 19:00:25 +0000

    Added the rest of the project

---
+++ ConfigureSitecore.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConfigureSitecore.cs" company="Sitecore Corporation">
+//   Copyright (c) Sitecore Corporation 1999-2017
+// </copyright>
+// <summary>
+//   The SamplePlugin startup class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Plugin.Sample.Condition
+{
+    using System.Reflection;
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    using Sitecore.Commerce.Core;
+    using Sitecore.Framework.Configuration;
+
+    public class ConfigureSitecore : IConfigureSitecore
+    {
+        /// <summary>
+        /// The configure services.
+        /// </summary>
+        /// <param name="services">
+        /// The services.
+        /// </param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            services.RegisterAllPipelineBlocks(assembly);
+        }
+    }
+}
---
+++ Services/WeatherService.cs
@@ -0,0 +1,30 @@
+﻿using OpenWeatherMap;
+using SendGrid.CSharp.HTTP.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Plugin.Sample.Condition.Services
+{
+    public class WeatherService
+    {
+        private string applicationId;
+
+        public WeatherService(string applicationId)
+        {
+            this.applicationId = applicationId; 
+        }
+
+        public async Task<Temperature> GetCurrentTemperature(string city, string country)
+        {
+            var client = new OpenWeatherMapClient(this.applicationId);
+
+            var currentWeather = await client.CurrentWeather.GetByName($"{city}, {country}", MetricSystem.Metric);
+
+            return currentWeather.Temperature;
+        }
+    }
+}
