2015b6c651a7de71634afd05a590cd428bbb3a71
Erwin Werkman <werkman.erwin@gmail.com>
2017-05-15 18:54:18 +0000

    v1

---
+++ Conditions/MinTemperatureCondition.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Sitecore.Commerce.Core;
+using Sitecore.Framework.Rules;
+using Sitecore.Commerce.Plugin.Carts;
+using Plugin.Sample.Condition.Services;
+using Plugin.Sample.Condition.Policies;
+
+namespace Plugin.Sample.Condition.Conditions
+{
+    public class MinTemperatureCondition : ICartsCondition, ICondition, IMappableRuleEntity 
+    {
+        public IRuleValue<Decimal> MinimumTemperature { get; set; }
+        public IRuleValue<String> City { get; set; }
+        public IRuleValue<String> Country { get; set; }
+
+        public bool Evaluate(IRuleExecutionContext context)
+        {
+            var minimumTemperature = MinimumTemperature.Yield(context);
+            var city = City.Yield(context);
+            var country = Country.Yield(context);
+
+            CommerceContext commerceContext = context.Fact<CommerceContext>((string)null);
+            var weatherServicePolicy = commerceContext.GetPolicy<WeatherServiceClientPolicy>();
+
+            var currentTemperature = GetCurrentTemperature(city, country, weatherServicePolicy.ApplicationId);
+
+            return currentTemperature > minimumTemperature;
+        }
+
+        public decimal GetCurrentTemperature(string city, string country, string applicationId)
+        {
+            WeatherService weatherService = new WeatherService(applicationId);
+            var temperature = weatherService.GetCurrentTemperature(city, country).Result;
+
+            return (decimal) temperature.Value;
+        }
+    }
+}
---
+++ Policies/WeatherServiceClientPolicy.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.Commerce.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Plugin.Sample.Condition.Policies
+{
+    public class WeatherServiceClientPolicy : Policy
+    {
+        public WeatherServiceClientPolicy()
+        {
+            this.ApplicationId = string.Empty;
+        }
+
+        public string ApplicationId { get; set; }
+    }
+}
