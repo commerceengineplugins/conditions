d665c5b967050059f4f9700141ef17caaeeed68b
Erwin Werkman <werkman.erwin@gmail.com>
2017-05-20 12:23:42 +0000

    Removed unnecessary usings

--- Services/WeatherService.cs
+++ Services/WeatherService.cs
@@ -1,9 +1,4 @@
 ﻿using OpenWeatherMap;
-using SendGrid.CSharp.HTTP.Client;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 
